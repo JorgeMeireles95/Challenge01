88e7e57c9f17b46474e5514e7249505caa7610fe
Jorge Meireles <jorgentoniomeireles@gmail.com>
2022-09-12 17:41:12 -0300

    metodo encontrar pelo id inplementado

--- src/main/java/com/alura/challenge/controller/VideoController.java
+++ src/main/java/com/alura/challenge/controller/VideoController.java
@@ -8,7 +8,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,12 +26,12 @@ import com.alura.challenge.repositories.VideoRepository;
 public class VideoController {
 
 	@Autowired
-	private VideoRepository repository;
+	private VideoRepository videoRepository;
 
 	@PostMapping
 	public ResponseEntity<VideoDto> cadastrar(@RequestBody @Valid VideoForm form,UriComponentsBuilder uriBuilder) {
 		Video video = form.converter();
-		repository.save(video);
+		videoRepository.save(video);
 		URI uri = uriBuilder.path("/videos/{id}").buildAndExpand(video.getvideoId()).toUri();
 		return ResponseEntity.created(uri).body(new VideoDto(video));
 
@@ -37,10 +39,19 @@ public class VideoController {
 
 	@GetMapping
 	public List<VideoDto> lista() {
-		List<Video> videos = repository.findAll();
+		List<Video> videos = videoRepository.findAll();
 		return VideoDto.converter(videos);
 	}
 
+	
+	
+	
+	@GetMapping("/{id}")
+	public VideoDto unicoElemento(@PathVariable("id") Long id){
+		Video video = videoRepository.getById(id);
+		
+		return new VideoDto(video);
+	}
 
 
 }
